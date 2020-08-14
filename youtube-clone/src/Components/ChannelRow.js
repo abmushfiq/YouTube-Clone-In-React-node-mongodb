fe15fff13368a3365d932b263028d7d86727b1e8
Mushfiq Ahmed <abmushfiq@gmail.com>
2020-08-14 19:59:20 +0530

    search page & Home page complete front end 80% work finished

--- youtube-clone/src/Components/ChannelRow.js
+++ youtube-clone/src/Components/ChannelRow.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import "./ChannelRow.css";
-import { Avatar } from "@material-ui/core";
+import Avatar  from "@material-ui/core/Avatar";
+import VerifiedIcon from "@material-ui/icons/CheckCircleOutline";
 
-function channelRow({image, channel, subs, noOfVideos, description, verified}) {
+function ChannelRow({image, channel, subs, noOfVideos, description, verified}) {
     return (
         <div className='channelRow'>
             <Avatar className='channelRow__logo' alt={channel} src={image} />
-            <div className ='channelRow__logo'>
-               <h4>{channel}</h4>
+            <div className ='channelRow__text'>
+            <h4>{channel} {verified && <VerifiedIcon />}</h4>
+            <p>{subs} subscription . {noOfVideos} videos</p>
+            <p>{description}</p>
             </div>
 
             
@@ -15,4 +18,4 @@ function channelRow({image, channel, subs, noOfVideos, description, verified}) {
     )
 }
 
-export default channelRow;
+export default ChannelRow;
--- youtube-clone/src/Pages/SearchPage.js
+++ youtube-clone/src/Pages/SearchPage.js
@@ -2,8 +2,11 @@ import React from 'react'
 import SideBar from "../Components/SideBar";
 import "./Search.css";
 import TuneIcon from "@material-ui/icons/Tune";
-import channelRow from "../Components/ChannelRow";
+import ChannelRow from "../Components/ChannelRow";
 import ChannelImg from "../images/youtube.png";
+import VidoeRow from '../Components/VideoRow'
+import VideoRow from '../Components/VideoRow';
+import thum2 from "../images/thum2.jpg";
 
 function SearchPage() {
     return (
@@ -17,12 +20,85 @@ function SearchPage() {
             </div>
             <hr />
 
-            <channelRow image= {ChannelImg}
+            <ChannelRow image= {ChannelImg}
             channel='Youtube'
-             subs
-              noOfVideos
-               description
-                verified 
+            verified
+             subs='660k'
+              noOfVideos={382}
+               description ='you can find and watch amazing videos and you can create your own content'
+                
+                />
+
+                <hr />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
+                />
+
+                <VideoRow
+                views='1.4M'
+                subs='659k'
+                description='Do you want to free training check thi video'
+                timestamp='59 second ago'
+                channel='youtube'
+                title='lets build you tube clone'
+                image={thum2}
                 />
             </div>
         </div>
